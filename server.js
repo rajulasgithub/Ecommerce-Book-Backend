d0e5b29bf40ed13dce596690daec338333bb2527
rajulasgithub <rajulasrazak688@gmail.com>
2025-12-09 15:51:33 +0530

    server file chnages

--- server.js
+++ server.js
@@ -17,12 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended:true}))
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000", 
-    credentials: true,              
-  })
-);
+app.use(cors());
 
 app.use('/uploads', express.static('uploads'));
 
